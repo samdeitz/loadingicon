4da20e87790328875f38aa8e93ceaaf5a9df4851
Sammy <101956263+samde742@users.noreply.github.com>
2025-09-25 22:27:43 -0400

    build for download

---
+++ LoadingIconCode/Circle.java
@@ -0,0 +1,14 @@
+/*
+ * Sam Deitz
+ * 5/23/2023
+ * growing circle class
+ */
+
+class Circle {
+    int x, y, size;
+    Circle(int x, int y, int size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+}
---
+++ LoadingIconCode/PointD.java
@@ -0,0 +1,16 @@
+/*
+ * Sam Deitz
+ * 5/23/2023
+ * exact point class
+ */
+
+public class PointD {
+    double x,y;
+
+    PointD() {}
+
+    PointD(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
